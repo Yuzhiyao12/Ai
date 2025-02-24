1602e1c4370dbff75a02961f2d34ccf40d443b0f
Yuzhiyao12 <120603051+Yuzhiyao12@users.noreply.github.com>
2025-02-24 16:13:00 +0800

    Add files via upload

--- route/app.php
+++ route/app.php
@@ -16,4 +16,4 @@ Route::get('/api/count', 'index/getCount');
 // 更新计数，自增或者清零
 Route::post('/api/count', 'index/updateCount');
 
-Route::post('/api/login', 'login/index');
+Route::post('/api/login', 'index/login');
